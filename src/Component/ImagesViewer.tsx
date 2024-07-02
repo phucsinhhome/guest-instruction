a2f21d526865d12be18701a19fa618dcff2eeed7
Minh Trần <wisekingtran@gmail.com>
2024-07-02 13:27:23 +0700

    Update ImagesViewer.tsx

--- src/Component/ImagesViewer.tsx
+++ src/Component/ImagesViewer.tsx
@@ -21,11 +21,11 @@ export const ImagesViewer = () => {
                     })}
                 </div>
                 <div className="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+                    {
+                        images.map((image, i) => {
+                            return (<button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label={i} data-carousel-slide-to={i}/>)
+                        })
+                    }
                 </div>
                 <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
                     <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
