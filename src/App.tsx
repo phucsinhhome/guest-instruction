dac3d35caefd386cae2ab10cd78225c8d3254dc7
Minh Trần <wisekingtran@gmail.com>
2024-07-03 16:33:15 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { ImagesViewer } from './Component/ImagesViewer'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const menuURL = 'https://via.placeholder.com/150'
-
+ 
   return (
     <>
       <ImagesViewer/>
